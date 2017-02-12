39d692dc921a80c5a446d69d921bf3d29115ade4
Govind Bhone <bhonegovind@gmail.com>
2017-02-12 09:32:56 -0500

    Added the scala version 2.11.4 instead of 2.10.3

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object Build extends Build {
 
   val commonSettings = Defaults.coreDefaultSettings ++ Seq(
     scalaVersion := "2.11.4",
-    crossScalaVersions := Seq("2.11.4", "2.10.4"),
+    crossScalaVersions := Seq("2.11.4", "2.10.3"),
     version := "0.1",
     crossPaths := false
   )
@@ -23,7 +23,8 @@ object Build extends Build {
         packMain := shellScripts,
         packBashTemplate := "template/launch.mustache",
         libraryDependencies ++= Seq("org.xerial" % "xerial-core" % "3.3.6",
-          "org.xerial.snappy" % "snappy-java" % "1.1.1.6")
+          "org.xerial.snappy" % "snappy-java" % "1.1.1.6",
+          "org.scalatest" % "scalatest_2.11" % "3.0.1")
       )
   ) dependsOn()
 
